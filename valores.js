4fee948d8990cd8b94396b5fb96d3b08452d1247
Andrés Rodríguez <darkknight64@gmail.com>
2020-04-19 15:20:30 -0500

    Add var session

---
+++ valores.js
@@ -0,0 +1,14 @@
+var nombre = "Oscar";
+
+// declarar
+var edad;
+
+// inicializar
+edad = 28;
+
+var elementos = ["Computador", "Celular"];
+
+var persona = {
+    nombre: "Andres",
+    edad: 28
+}
